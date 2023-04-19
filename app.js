18f0b60a359e98cbf2719b1c61dab846dcb317be
Luke Harding <lmharding04@gmail.com>
2023-04-19 13:29:11 +0100

    update / Navbar and JS

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
 "use strict";
+/*=========
+Nav Bar >>>
+==========*/
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
 
 const button = document.getElementById("generate-idea");
-
+/*=================
+GENRE GENERATOR >>>
+=================*/
 const showThisMessage = function (message) {
   let displayArea = document.getElementById("display-idea");
   displayArea.innerHTML = message;
 };
 
-const makeAndShowIdea = function () {
-  let randomConcept = getIdea();
-  showThisMessage(randomConcept);
+const showConcept = function () {
+  let randomGenre = getConcept();
+  showThisMessage(randomGenre);
 };
 
 const pickRandom = function (list) {
   return list[Math.floor(Math.random() * list.length)];
 };
 
-button.addEventListener("click", makeAndShowIdea);
+button.addEventListener("click", showConcept);
 
-const getIdea = function () {
-  let idea = [];
-  idea.push("Magic");
-  idea.push("Apocolypse");
-  idea.push("Time Travel");
-  idea.push("Space");
+const getConcept = function () {
+  let genre = [];
+  genre.push("Fantasy");
+  genre.push("Adventure");
+  genre.push("Romance");
+  genre.push("Mystery");
+  genre.push("Horror");
+  genre.push("Science Fiction");
+  genre.push("History");
+  genre.push("Thriller");
 
-  const generatedIdea = "Idea Generated = " + pickRandom(idea);
-  console.log("Idea Generated = " + generatedIdea);
-  return generatedIdea;
+  let theme = [];
+  theme.push("Fantasy");
+
+  const generatedGenre =
+    "Genre = " + pickRandom(genre) + " / " + "Theme = " + pickRandom(theme);
+  console.log("Genre = " + generatedGenre);
+  return generatedGenre;
 };
---
+++ user-stories.md
@@ -0,0 +1,16 @@
+# User Stories
+
+1. As a Novel writer, I wan't to have a large variety of idea's that i can use for inspiration for what I am to write about next.
+
+2. As an Artist, I wan't many different wacky and fonderful idea's that i can draw and sketch out as practice.
+
+3. As a Game developer I wan't many idea's with different concepts to generate so that i can get a better understanding of what i want to make.
+
+## Establish MVP
+
+1. MUST! Generate idea's!
+
+## Stretch Goals
+
+1. Have an idea generator for different things e.g: Novel idea generator, website idea generator, movie idea etc... (Novel Gen = Genre, Concept, Theme.)
+2. Make the site look decent.
