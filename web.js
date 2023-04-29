c5766d5b71647bf41192df15dcb065fd42c6b4bc
Luke Harding <lmharding04@gmail.com>
2023-04-29 19:14:02 +0100

    updated

--- web.js
+++ web.js
@@ -11,3 +11,32 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+/* IDEA GENERATOR */
+
+const button = document.getElementById("generate-idea");
+
+const showThisMessage = function (message) {
+  let displayArea = document.getElementById("display-idea");
+  displayArea.innerHTML = message;
+};
+
+const showWebsite = function () {
+  let randomWebsite = getWebsite();
+  showThisMessage(randomWebsite);
+};
+
+const pickRandom = function (list) {
+  return list[Math.floor(Math.random() * list.length)];
+};
+
+button.addEventListener("click", showWebsite);
+
+const getWebsite = function () {
+  let website = [];
+  website.push("Make a webiste to spread awareness of indangered hobbits");
+
+  const generatedWebsite = "Website Idea = " + pickRandom(website);
+  console.log("Generated Idea" + generatedWebsite);
+  return generatedWebsite;
+};
